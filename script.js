5726f387969b51438f4967975e19ae93421471d7
pwdrc <tolvo.pedro@gmail.com>
2024-03-27 20:58:14 -0300

    add: hamburguer

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// Texto do código em C
-const codeText = `
-oi! eu sou o pedro
-
-esse é meu cantinho na web
-
-fica à vontade e não repara na bagunça ^^
-`;
-
-const runCodeText = `
-pedro@ubuntu:~$ gcc -o hello hello.c
-pedro@ubuntu:~$ ./
-hello
-olá, mundo
-pedro@ubuntu:~$`;
-
-// Captura o elemento de código
-
-
 function altNav() {
   const nav = document.getElementsByClassName("nav")[0];
 
